cb0cd0969461ae78f089764951d9d21c8f5a47e2
Vojto Rinik <vojto@rinik.net>
2011-09-19 10:43:14 +0200

    Initial Commit

---
+++ RKSyntaxView/RKSyntaxView.h
@@ -0,0 +1,13 @@
+//
+//  RKSyntaxView.h
+//  RKSyntaxView
+//
+//  Created by Vojto Rinik on 9/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RKSyntaxView : NSObject
+
+@end
---
+++ RKSyntaxView/RKSyntaxView.m
@@ -0,0 +1,23 @@
+//
+//  RKSyntaxView.m
+//  RKSyntaxView
+//
+//  Created by Vojto Rinik on 9/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RKSyntaxView.h"
+
+@implementation RKSyntaxView
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
